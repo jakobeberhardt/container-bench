1a163845512e0635c68e5554e3203d38e656434c
Jakob Eberhardt <76037736+jakobeberhardt@users.noreply.github.com>
2025-10-27 11:09:38 +0100

    post setup steps
    Added instructions for setting up Go in the shell profile.

--- setup/tools/post-setup.sh
+++ setup/tools/post-setup.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+sudo chown -R $USER: .
 # Add user to docker group to run docker without sudo
 sudo usermod -aG docker $USER
 
 # Add Go to PATH
 echo 'export PATH=/usr/local/go/bin:$PATH' >> $HOME/.bashrc
+echo ""
+echo "To use Go, add the following to your shell profile (~/.bashrc, ~/.zshrc, etc.):"
+echo "export PATH=/usr/local/go/bin:\$PATH"
+echo ""
+echo "Or run the following command in your current CloudLab session:"
+echo "echo "export PATH=/usr/local/go/bin:\$PATH"" >> /user/$USER/.bashrc"
+echo ""
+echo "Verify installation with: go version"
