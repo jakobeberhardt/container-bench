4b07caa08be3c14a26f6f7025ce10309c5ae45ac
Jakob Eberhardt <76037736+jakobeberhardt@users.noreply.github.com>
2025-10-27 11:02:16 +0100

    Remove checkout

--- setup/get-container-bench.sh
+++ setup/get-container-bench.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 git clone https://github.com/jakobeberhardt/container-bench.git
 cd ./container-bench
-git checkout refactor
-
 sudo bash setup/install-setup.sh
-make 
+
+echo "Run "make" or "make install""
