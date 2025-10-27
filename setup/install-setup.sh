9e8051043de3791d7561ed09e10bfbd5bc3913c7
eberhard <eberhard@node-0.eberhard-275806.rdt4nn-pg0.clemson.cloudlab.us>
2025-10-27 07:44:10 -0400

    Adds stress-ng

--- setup/install-setup.sh
+++ setup/install-setup.sh
@@ -4,4 +4,4 @@ sudo bash setup/tools/update.sh
 sudo bash setup/tools/install-docker.sh
 sudo bash setup/tools/install-collectors.sh
 sudo bash setup/tools/install-go.sh
-sudo bash setup/tools/post-setup.sh
+bash setup/tools/post-setup.sh
