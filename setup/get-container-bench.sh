81915550d35a1b4fabacbc54da390d5d80d79948
eberhard <eberhard@node-0.eberhard-275796.rdt4nn-pg0.clemson.cloudlab.us>
2025-10-27 07:27:14 -0400

    Adds steps

--- setup/get-container-bench.sh
+++ setup/get-container-bench.sh
@@ -4,3 +4,7 @@ cd ./container-bench
 sudo bash setup/install-setup.sh
 
 echo "Run "make" or "make install""
+echo "and run e.g."
+echo ""
+echo 'sudo container-bench validate -c examples/simple_test.yml'
+echo 'sudo container-bench run -c examples/simple_test.yml'
