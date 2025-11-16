d2c788e15fd0d20c415037e7f2a67907c6adcca0
eberhard <eberhard@server.eberhard-278920.rdt4nn-pg0.wisc.cloudlab.us>
2025-11-16 07:01:45 -0600

    Adds polar mappings

--- internal/plot/polar/mappings/style.go
+++ internal/plot/polar/mappings/style.go
@@ -8,102 +8,23 @@ type PolarPlotStyle struct {
 }
 
 var ProbeStyles = []PolarPlotStyle{
-	{
-		Color:     "red",
-		LineStyle: "solid",
-		LineWidth: "thick",
-		Mark:      "none",
-	},
-	{
-		Color:     "blue",
-		LineStyle: "solid",
-		LineWidth: "thick",
-		Mark:      "none",
-	},
-	{
-		Color:     "green!60!black",
-		LineStyle: "solid",
-		LineWidth: "thick",
-		Mark:      "none",
-	},
-	{
-		Color:     "orange",
-		LineStyle: "solid",
-		LineWidth: "thick",
-		Mark:      "none",
-	},
-	{
-		Color:     "purple",
-		LineStyle: "solid",
-		LineWidth: "thick",
-		Mark:      "none",
-	},
-	{
-		Color:     "cyan",
-		LineStyle: "solid",
-		LineWidth: "thick",
-		Mark:      "none",
-	},
-	{
-		Color:     "magenta",
-		LineStyle: "solid",
-		LineWidth: "thick",
-		Mark:      "none",
-	},
-	{
-		Color:     "olive",
-		LineStyle: "solid",
-		LineWidth: "thick",
-		Mark:      "none",
-	},
-	{
-		Color:     "teal",
-		LineStyle: "solid",
-		LineWidth: "thick",
-		Mark:      "none",
-	},
-	{
-		Color:     "brown",
-		LineStyle: "solid",
-		LineWidth: "thick",
-		Mark:      "none",
-	},
-	{
-		Color:     "violet",
-		LineStyle: "solid",
-		LineWidth: "thick",
-		Mark:      "none",
-	},
-	{
-		Color:     "pink",
-		LineStyle: "solid",
-		LineWidth: "thick",
-		Mark:      "none",
-	},
-	{
-		Color:     "lime",
-		LineStyle: "solid",
-		LineWidth: "thick",
-		Mark:      "none",
-	},
-	{
-		Color:     "red!70!black",
-		LineStyle: "solid",
-		LineWidth: "thick",
-		Mark:      "none",
-	},
-	{
-		Color:     "blue!70!black",
-		LineStyle: "solid",
-		LineWidth: "thick",
-		Mark:      "none",
-	},
-	{
-		Color:     "gray!70!black",
-		LineStyle: "solid",
-		LineWidth: "thick",
-		Mark:      "none",
-	},
+	{Color: "red", LineStyle: "solid", LineWidth: "thick", Mark: "none"},
+	{Color: "blue!55!black", LineStyle: "solid", LineWidth: "thick", Mark: "none"},
+	{Color: "green!55!black", LineStyle: "solid", LineWidth: "thick", Mark: "none"},
+	{Color: "orange", LineStyle: "solid", LineWidth: "thick", Mark: "none"},
+	{Color: "purple", LineStyle: "solid", LineWidth: "thick", Mark: "none"},
+	{Color: "teal", LineStyle: "solid", LineWidth: "thick", Mark: "none"},
+	{Color: "pink!80!red", LineStyle: "solid", LineWidth: "thick", Mark: "none"},
+	{Color: "blue!20!violet", LineStyle: "solid", LineWidth: "thick", Mark: "none"},
+	
+	{Color: "orange", LineStyle: "densely dotted", LineWidth: "thick", Mark: "none"},
+	{Color: "green!70!black", LineStyle: "densely dotted", LineWidth: "thick", Mark: "none"},
+	{Color: "blue!50!white", LineStyle: "densely dotted", LineWidth: "thick", Mark: "none"},
+	{Color: "teal", LineStyle: "densely dotted", LineWidth: "thick", Mark: "none"},
+	{Color: "violet", LineStyle: "densely dotted", LineWidth: "thick", Mark: "none"},
+	{Color: "olive!50!black", LineStyle: "densely dotted", LineWidth: "thick", Mark: "none"},
+	{Color: "pink!80!red", LineStyle: "densely dotted", LineWidth: "thick", Mark: "none"},
+	{Color: "purple", LineStyle: "densely dotted", LineWidth: "thick", Mark: "none"},
 }
 
 func GetProbeStyle(probeIndex int) PolarPlotStyle {
